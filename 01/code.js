be738c1bc26f495f0040b237a8100b389449f880
baliddim <baliddim@zhaw.students.ch>
2019-12-01 21:23:27 +0100

    Add day 1 part 1

---
+++ 01/code.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var text = fs.readFileSync("./data.txt").toString('utf-8');
+var textByLine = text.split("\n");
+
+var sum = 0;
+// textByLine[0] = parseInt(textByLine[0]);
+
+console.log(Math.floor(textByLine[0]/3)-2);
+
+for(i=0; i < textByLine.length; i++){
+    textByLine[i] = parseInt(textByLine[i]);
+    textByLine[i] = Math.floor(textByLine[i]/3)-2;
+    sum += textByLine[i];
+}
+console.log(sum);
